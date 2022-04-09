69dc269ba65fc083346a719b32bfb5f9b58ce9f2
Aniruddha <aniruddha1712@gmail.com>
2022-04-09 16:54:52 +0530

    [Aniruddha]Added: Created simple LinkedList using addlast method

--- LinkedList/Program.cs
+++ LinkedList/Program.cs
@@ -11,6 +11,13 @@ namespace LinkedList
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the LinkedList Program\n");
+
+            MyLinkedList linkedList = new MyLinkedList();
+            linkedList.AddLast(56);
+            linkedList.AddLast(30);
+            linkedList.AddLast(70);
+            linkedList.Display();
+
             Console.ReadLine();
         }   
     }
